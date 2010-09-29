d445f0a049e5dca347fcf66931ded924750e6d3e
Tomasz Figa <tomasz.figa@gmail.com>
2010-09-29 16:44:52 +0000

    Add missing copyrights.

--- libsgl/fimg/fragment.c
+++ libsgl/fimg/fragment.c
@@ -1,11 +1,23 @@
 /*
-* fimg/fragment.c
-*
-* SAMSUNG S3C6410 FIMG-3DSE PER FRAGMENT UNIT RELATED FUNCTIONS
-*
-* Copyrights:	2005 by Samsung Electronics Limited (original code)
-*		2010 by Tomasz Figa <tomasz.figa@gmail.com> (new code)
-*/
+ * fimg/fragment.c
+ *
+ * SAMSUNG S3C6410 FIMG-3DSE PER FRAGMENT UNIT RELATED FUNCTIONS
+ *
+ * Copyrights:	2010 by Tomasz Figa < tomasz.figa at gmail.com >
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "fimg_private.h"
 
