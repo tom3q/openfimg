97eccac759827119099722dfe15a17dbeff80ca0
Tomasz Figa <tomasz.figa@gmail.com>
2012-03-13 17:04:16 +0100

    fglsurface: Rework FGLLocalSurface constructor.

--- libsgl/fglsurface.cpp
+++ libsgl/fglsurface.cpp
@@ -47,53 +47,45 @@
  * Surfaces
  */
 
-FGLLocalSurface::FGLLocalSurface(unsigned long size)
+FGLLocalSurface::FGLLocalSurface(unsigned long req_size)
 	: fd(-1)
 {
-	int fd;
-	void *vaddr;
 	pmem_region region;
 	unsigned long page_size = getpagesize();
-	unsigned long rounded;
 
-	rounded = (size + page_size - 1) & ~(page_size - 1);
+	/* Round up to page size */
+	size = (req_size + page_size - 1) & ~(page_size - 1);
 
-	// create a buffer file (cached)
+	/* Create a buffer file (cached) */
 	fd = open("/dev/pmem_gpu1", O_RDWR, 0);
 	if(fd < 0) {
 		LOGE("EGL: Could not open PMEM device (%s)", strerror(errno));
 		return;
 	}
 
-	// allocate and map the memory
-	if ((vaddr = mmap(NULL, rounded, PROT_WRITE | PROT_READ,
-				MAP_SHARED, fd, 0)) == MAP_FAILED) {
+	/* Allocate and map the memory */
+	vaddr = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+	if (vaddr == MAP_FAILED) {
 		LOGE("EGL: PMEM buffer allocation failed (%s)", strerror(errno));
 		goto err_mmap;
 	}
 
+	/* Get physical address */
 	if (ioctl(fd, PMEM_GET_PHYS, &region) < 0) {
 		LOGE("EGL: PMEM_GET_PHYS failed (%s)", strerror(errno));
 		goto err_phys;
 	}
+	this->paddr = region.offset;
 
-	/* Clear the buffer (NOTE: Is it needed?) */
-	memset((char*)vaddr, 0, rounded);
-
-	/* Setup surface struct */
-	this->size	= rounded;
-	this->fd	= fd;
-	this->vaddr	= vaddr;
-	this->paddr	= region.offset;
-
-	flush();
-
+	/* Allocation succeeded */
 	return;
 
 err_phys:
-	munmap(vaddr, rounded);
+	munmap(vaddr, size);
 err_mmap:
 	close(fd);
+	/* Allocation failed */
+	fd = -1;
 }
 
 FGLLocalSurface::~FGLLocalSurface()
